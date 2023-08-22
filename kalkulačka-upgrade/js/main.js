87cd366b27ef9a354d42b04a3c2bd8d65a4ca5a6
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-08-23 00:41:43 +0200

    Add files via upload
    Have been added login adn password.

--- kalkulačka-upgrade/js/main.js
+++ kalkulačka-upgrade/js/main.js
@@ -4,3 +4,35 @@ import { setupPagination } from "./pagination.js";
 const historyPages = document.querySelectorAll(".history-page");
 setupPagination(historyPages);
 initializeCalculator();
+
+const loginForm = document.querySelector(".login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+const container = document.querySelector(".container");
+
+const loginInfo = [
+  { username: "martin", password: "pes" },
+  { username: "jana", password: "kočka" },
+  { username: "zdenda", password: "špaček" },
+  { username: "tonda", password: "čáp" },
+  // Další přihlašovací údaje
+];
+
+loginButton.addEventListener("click", () => {
+  const username = usernameInput.value.toLowerCase();
+  const password = passwordInput.value;
+  container.style.opacity = 1;
+
+  const validLogin = loginInfo.some(
+    (info) => info.username === username && info.password === password
+  );
+
+  if (validLogin) {
+    loginForm.style.display = "none";
+    container.style.display = "block";
+    container.style.pointerEvents = "";
+  } else {
+    alert("Invalid username or password");
+  }
+});
