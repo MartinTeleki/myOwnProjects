f21490b5d0d4d1bf344d6b4c141da6c110cfa4d5
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-08-23 11:55:15 +0200

    Add files via upload
    Login have been upgraded.

--- myOwnProjects-main/myOwnProjects-main/kalkulačka-upgrade/js/main.js
+++ myOwnProjects-main/myOwnProjects-main/kalkulačka-upgrade/js/main.js
@@ -5,37 +5,49 @@ const historyPages = document.querySelectorAll(".history-page");
 setupPagination(historyPages);
 initializeCalculator();
 
-const loginForm = document.querySelector(".login-form");
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-// const loginButton = document.getElementById("login-button");
-const container = document.querySelector(".container");
+// const loginForm = document.querySelector(".login-form");
+// const usernameInput = document.getElementById("username");
+// const passwordInput = document.getElementById("password");
 
-// const loginInfo = [
-//   { username: "martin", password: "pes" },
-//   { username: "jana", password: "kočka" },
-//   { username: "zdenda", password: "špaček" },
-//   { username: "tonda", password: "čáp" },
-//   // Další přihlašovací údaje
-// ];
+const container = document.querySelector(".container")
+const container1 = document.querySelector(".container-1");
+const nameInput = document.querySelector("#name")
+const passwordInput = document.querySelector("#password")
+const loginButton = document.querySelector(".btn-login")
+const loginForm = document.querySelector(".form-control-1")
+console.log(loginButton);
+//console.log(loginButton);
+const loginInfo = [
+  { username: "martin", password: "pes" },
+  { username: "jana", password: "kočka" },
+  { username: "zdenda", password: "špaček" },
+  { username: "tonda", password: "čáp" },
+  // Další přihlašovací údaje
+];
 
-// loginButton.addEventListener("click", () => {
-//   const username = usernameInput.value.toLowerCase();
-//   const password = passwordInput.value;
-//   container.style.opacity = 1;
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  const username =nameInput.value.toLowerCase();
+  const password = passwordInput.value;
+  console.log(username, password);
 
-//   const validLogin = loginInfo.some(
-//     (info) => info.username === username && info.password === password
-//   );
+  const validLogin = loginInfo.some(
+    (info) => info.username === username && info.password === password
+  );
 
-//   if (validLogin) {
-//     loginForm.style.display = "none";
-//     container.style.display = "block";
-//     container.style.pointerEvents = "";
-//   } else {
-//     alert("Invalid username or password");
-//   }
-// });
+  console.log(validLogin);
+
+  if (validLogin) {
+    loginForm.style.display = "none";
+    container.style.display = "block";
+    container.style.pointerEvents = "";
+    container1.style.opacity = 0
+    container.style.opacity = 1
+    
+  } else {
+    alert("Invalid username or password");
+  }
+});
 
 const labels = document.querySelectorAll(".form-control-1 label");
 
