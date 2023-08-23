1ded4387038061e269714e83eb3d5b9afe65c1f6
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-08-23 12:16:35 +0200

    Add files via upload
    Fixed bugs in login adn added OOP.

--- myOwnProjects-main/kalkulačka-upgrade/js/login.js
+++ myOwnProjects-main/kalkulačka-upgrade/js/login.js
@@ -1,4 +1,38 @@
-// function enableInteraction() {
-//   document.querySelector(".container").style.pointerEvents = "auto";
-//   document.querySelector(".history").style.pointerEvents = "auto";
-// }
+
+export function initializeLogin() {
+  const loginButton = document.querySelector(".btn-login");
+  const nameInput = document.querySelector("#name");
+  const passwordInput = document.querySelector("#password");
+  const loginForm = document.querySelector(".form-control-1");
+  const container = document.querySelector(".container");
+  const container1 = document.querySelector(".container-1");
+
+  const loginInfo = [
+    { username: "martin", password: "pes" },
+    { username: "jana", password: "kočka" },
+    { username: "zdenda", password: "špaček" },
+    { username: "tonda", password: "čáp" },
+    
+  ];
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = nameInput.value.toLowerCase();
+    const password = passwordInput.value;
+
+    const validLogin = loginInfo.some(
+      (info) => info.username === username && info.password === password
+    );
+
+    if (validLogin) {
+      loginForm.style.display = "none";
+      container.style.display = "block";
+      container.style.pointerEvents = "";
+      container1.style.opacity = 0;
+      container1.style.zIndex = -1000;
+      container.style.opacity = 1;
+    } else {
+      alert("Invalid username or password");
+    }
+  });
+}
