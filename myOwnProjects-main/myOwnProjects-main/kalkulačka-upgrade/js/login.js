0f993b203795d5a00f05c5da9fa53644e123ba3f
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-08-25 19:20:56 +0200

    Delete myOwnProjects-main directory

--- myOwnProjects-main/myOwnProjects-main/kalkulačka-upgrade/js/login.js
+++
@@ -1,4 +0,0 @@
-// function enableInteraction() {
-//   document.querySelector(".container").style.pointerEvents = "auto";
-//   document.querySelector(".history").style.pointerEvents = "auto";
-// }
